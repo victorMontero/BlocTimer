ea2d124235b354a73bde5f8ed9c5652f76cdc962
Victor Montero <viiktoor@gmail.com>
2020-10-28 18:56:02 -0400

    states were created

---
+++ lib/bloc/timer_state.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+
+abstract class TimerState extends Equatable {
+  final int duration;
+
+  TimerState(this.duration);
+
+  @override
+  List<Object> get props => [duration];
+
+}
+
+class TimerInitial extends TimerState {
+  TimerInitial(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerInitial {duration: $duration}';
+}
+
+class TimerRunPause extends TimerState {
+  TimerRunPause(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerRunPause {duration: $duration}';
+}
+
+class TimerRunInProgress extends TimerState {
+  TimerRunInProgress(int duration) : super(duration);
+
+  @override
+  String toString() => 'TimerRunInProgress {duration: $duration}';
+}
+
+class TimerRunComplete extends TimerState {
+  TimerRunComplete() : super(0);
+}
+
